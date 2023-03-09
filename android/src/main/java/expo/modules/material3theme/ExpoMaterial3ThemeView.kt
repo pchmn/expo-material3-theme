424579f69c325063c7e0fe6d6ca9fd4f6f785911
pchmn <pchmn.dev@gmail.com>
2023-03-09 18:42:53 +0100

    clean project

--- android/src/main/java/expo/modules/material3theme/ExpoMaterial3ThemeView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.material3theme
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ExpoMaterial3ThemeView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
 
 // Import the native module. On web, it will be resolved to ExpoMaterial3Theme.web.ts
 // and on native platforms to ExpoMaterial3Theme.ts
-import ExpoMaterial3ThemeModule from './ExpoMaterial3ThemeModule';
-import ExpoMaterial3ThemeView from './ExpoMaterial3ThemeView';
-import { ChangeEventPayload, ExpoMaterial3ThemeViewProps } from './ExpoMaterial3Theme.types';
+import ExpoMaterial3ThemeModule from "./ExpoMaterial3ThemeModule";
 
-// Get the native constant value.
-export const PI = ExpoMaterial3ThemeModule.PI;
 
 export function hello(): string {
   return ExpoMaterial3ThemeModule.hello();
 }
-
-export async function setValueAsync(value: string) {
-  return await ExpoMaterial3ThemeModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoMaterial3ThemeModule ?? NativeModulesProxy.ExpoMaterial3Theme);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoMaterial3ThemeView, ExpoMaterial3ThemeViewProps, ChangeEventPayload };
