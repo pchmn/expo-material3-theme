0d05454b871bbf3fec3d94c4df586639e36c32b3
pchmn <pchmn.dev@gmail.com>
2023-03-29 19:39:12 +0200

    lint: update commit lint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'lint'],
+      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'lint', 'example', 'chore'],
     ],
   },
 };
