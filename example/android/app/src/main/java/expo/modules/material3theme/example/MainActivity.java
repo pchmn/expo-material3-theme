8cbeabe8629e2bf4009ea5bc5f55f5b3af8bae48
pchmn <pchmn.dev@gmail.com>
2023-07-07 17:25:10 +0200

    example: upgrade expo sdk to v49

--- example/android/app/src/main/java/expo/modules/material3theme/example/MainActivity.java
+++ example/android/app/src/main/java/expo/modules/material3theme/example/MainActivity.java
@@ -1,4 +1,4 @@
-package com.expomaterial3themeexample;
+package expo.modules.material3theme.example;
 
 import android.os.Build;
 import android.os.Bundle;
@@ -40,10 +40,7 @@ public class MainActivity extends ReactActivity {
         this,
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
-        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
-        ));
+        DefaultNewArchitectureEntryPoint.getFabricEnabled()));
   }
 
   /**
